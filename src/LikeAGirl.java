51ebd5021742cdcc18b0fe0462cd44843015ce51
Atticus Blanco <atticusbrujx@gmail.com>
2023-09-02 20:00:18 -0700

    Instructor's comments removed

--- src/LikeAGirl.java
+++ src/LikeAGirl.java
@@ -6,7 +6,7 @@ public class LikeAGirl {
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
-        // String variables, some with format specifiers
+
         String scriptTemplateLine1 = "What does it mean to do something, \"%s\"?";
         String scriptTemplateLine2 = "Show me what it looks like to run, \"%s.\"%n";
         String scriptTemplateLine3 = "Show me what it looks like to fight, \"%s.\"";
@@ -17,27 +17,17 @@ public class LikeAGirl {
         String ansBad = "Good for you.%nEmotional Damage %f";
         String likeAGirl = "Like a Girl";
 
-        // integer variable
+
         int good = 0;
         int bad = 1;
-        // 32 bit floating point variable
-        float emotionalDamage = 0.0f;  // 32 Bit, but it does exist!
-        // double precision floating point variable
-        double dEmotionalDamage = 100.0; // Double precision
-        // boolean variable
+        float emotionalDamage = 0.0f;
+        double dEmotionalDamage = 100.0;
         boolean trueOrFalse;
-        // Scanner variable for reading input.
         Scanner s = new Scanner(System.in);
 
-        System.out.println(
-                String.format(scriptTemplateLine1, likeAGirl));
-
-        // Example of using printf and platform specific line separator "%n" to
-        // format instead of String.format
+        System.out.println(String.format(scriptTemplateLine1, likeAGirl));
         System.out.printf(scriptTemplateLine2, likeAGirl);
-
         System.out.println(String.format(scriptTemplateLine3, likeAGirl));
-
         System.out.printf(scriptTemplateLine4, likeAGirl, good, bad);
 
         int answer = Integer.parseInt(s.nextLine());
@@ -49,7 +39,7 @@ public class LikeAGirl {
                 )
         );
 
-        trueOrFalse = (answer != good);  // if answer == 1 (i.e. good), then trueOrFalse should be False
+        trueOrFalse = (answer != good);
         System.out.printf("Did you answer like a nice person? %B%n", trueOrFalse);
     }
 }
